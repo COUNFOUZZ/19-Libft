3b62fdcc7ab06832c8e7162086b4ec1e53f448f9
Alexis Abda <alexis.abda@hotmail.com>
2022-04-06 21:07:14 +0200

    :sparkles: add ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (!dst || !src)
+		return (0);
+	j = ft_strlen(src);
+	while (dstsize <= 0)
+		return (j);
+	while (i < dstsize - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (j);
+}
