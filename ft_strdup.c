af5fe705fe2e5b5869783ed1c9c255719ed51396
Alexis Abda <aabda@student.s19.be>
2022-07-04 12:38:02 +0200

    norminette

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 12:27:12 by aabda             #+#    #+#             */
+/*   Updated: 2022/07/04 12:32:57 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    char    *str;
-    int     i;
+	char	*str;
+	int		i;
 
-    str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-    if (!str)
-        return (0);
-    i = 0;
-    while (s[i])
-    {
-        str[i] = s[i];
-        i++;
-    }
-    str[i] = '\0';
-    return (str);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
--- libft.h
+++ libft.h
@@ -1,16 +1,28 @@
-#ifndef libft_h
-#   define libf_h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 11:58:38 by aabda             #+#    #+#             */
+/*   Updated: 2022/07/04 12:36:26 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdlib.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
-int	    ft_isalnum(int c);
-int	    ft_isalpha(int c);
-int	    ft_isascii(int c);
-int	    ft_isdigit(int c);
-int	    ft_isprint(int c);
-int	    ft_tolower(int c);
-int	    ft_toupper(int c);
-char    *ft_strdup(const char *s);
+# include <stdlib.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
 
