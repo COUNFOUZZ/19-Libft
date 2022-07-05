85e36d15a91e12c8efbe17bd89f55be840272712
Alexis Abda <aabda@student.s19.be>
2022-07-05 15:15:31 +0200

    add ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 14:42:22 by aabda             #+#    #+#             */
+/*   Updated: 2022/07/05 15:14:05 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)(s + i));
+		i++;
+	}
+	return (NULL);
+}
+
+// int	main(void)
+// {
+// 	char *str = "Salut mon ] pote !";
+
+// 	printf("%s\n", strchr(str, 'l'));
+// 	printf("%s\n\n", ft_strchr(str, 'l'));
+
+// 	printf("%s\n", strchr(str, ' '));
+// 	printf("%s\n\n", ft_strchr(str, ' '));
+
+// 	printf("%s\n", strchr(str, ']'));
+// 	printf("%s\n\n", ft_strchr(str, ']'));
+
+// 	printf("%s\n", strchr(str, 'z'));
+// 	printf("%s\n", ft_strchr(str, 'z'));
+
+// 	return (0);
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:58:38 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/04 18:50:34 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/05 15:14:46 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
