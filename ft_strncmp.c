0439549e8d4aab9a1e543261c905d98463a83833
Alexis Abda <alexis.abda@hotmail.com>
2022-07-07 00:10:25 +0200

    Fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 20:16:37 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/06 20:30:32 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/07 00:07:52 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (i < n && (s1[i] || s2[i]))
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
+		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
 
 // int	main(void)
 // {
-// 	char s1[] = "Alexis";
-// 	char s2[] = "Salut poe !";
+// 	char    s1[] = "Salut pote !";
+//     char    s2[] = "Sut pote !";
 
 // 	printf("%d\n", strncmp(s1, s2, 10));
 // 	printf("%d\n", ft_strncmp(s1, s2, 10));
