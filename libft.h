16ba6f75357bf0865b3cb197ec15fca7f999d75a
Alexis Abda <alexis.abda@hotmail.com>
2022-07-09 14:52:07 +0200

    add ft_putendl_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:58:38 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/09 12:07:10 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/09 14:51:29 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void    ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
