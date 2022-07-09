188f45a8cc4f29099e8eeaa0ae1722162128cf27
Alexis Abda <alexis.abda@hotmail.com>
2022-07-09 11:58:24 +0200

    add ft_putchar_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:58:38 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/07 20:08:21 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/09 11:57:52 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_toupper(int c);
 int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void    ft_putchar_fd(char c, int fd);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
