dc5404ece7ee8b0c289495750b3ce92ecee39cc6
Alexis Abda <aabda@student.s19.be>
2022-07-10 16:06:22 +0200

    Fix prototype

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,23 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 14:06:13 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/10 14:27:45 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/10 16:05:22 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	// if (!src || !dest)
+	// if (!src || !dst)
 	// 	return (NULL);
 	while (i < n)
 	{
-		((unsigned char *)dest)[i] = ((unsigned const char *)src)[i];
+		((unsigned char *)dst)[i] = ((unsigned const char *)src)[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
