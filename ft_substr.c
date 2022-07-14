4008f44b9dfe3ae151beba2b5b397663a3659581
Alexis Abda <aabda@student.s19.be>
2022-07-14 17:57:39 +0200

    add ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/14 17:26:45 by aabda             #+#    #+#             */
+/*   Updated: 2022/07/14 17:56:56 by aabda            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[start] && (i < len))
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	return (str);
+}
