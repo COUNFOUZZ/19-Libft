4362d1823d135c1bdd8b24e6fd1dfbae2a318669
Alexis Abda <aabda@student.s19.be>
2022-07-18 13:34:18 +0200

    fix ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 23:09:00 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/07 14:25:13 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 13:33:11 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	return (ft_strncmp((const char *)s1, (const char *)s2, n));
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }
