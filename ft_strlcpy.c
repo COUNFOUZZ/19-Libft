8a6a8549c5b0e4f2d65f997393fa33bd88816259
Alexis Abda <aabda@student.s19.be>
2022-07-18 15:31:32 +0200

    Fix

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,28 +6,24 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:58:11 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/04 16:30:14 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 15:30:03 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, char *src, size_t size)
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	if (!src || !dest)
-		return (0);
-	j = ft_strlen(src);
-	if (size <= 0)
-		return (j);
-	while (i < size - 1 && src[i])
+	if (dstsize <= 0)
+		return (ft_strlen(src));
+	while (i < dstsize - 1 && src[i])
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (j);
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
