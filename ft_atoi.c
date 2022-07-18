f972777e9adc68bbf5e03e0f2aaa69c8d56f5953
Alexis Abda <aabda@student.s19.be>
2022-07-18 12:52:38 +0200

    protect max and min value

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 17:48:34 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/13 17:07:42 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 12:50:28 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_atoi(const char *str)
 {
-	size_t	i;
-	int		sign;
-	int		result;
+	size_t		i;
+	int			sign;
+	long int	result;
 
 	i = 0;
 	sign = 1;
@@ -33,6 +33,10 @@ int	ft_atoi(const char *str)
 	{
 		result = result * 10 + str[i] - '0';
 		i++;
+		if (result * sign < INT_MIN)
+			return (0);
+		if (result * sign > INT_MAX)
+			return (-1);
 	}
 	return (result * sign);
 }
