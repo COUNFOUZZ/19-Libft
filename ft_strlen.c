c187855461eb334f3ba1be9e243f5b1cc1321c5a
Alexis Abda <aabda@student.s19.be>
2022-07-18 13:10:48 +0200

    not need protection

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:58:17 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/17 17:11:10 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 13:09:56 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	while (s && s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
