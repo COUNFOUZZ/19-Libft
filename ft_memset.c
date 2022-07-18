efb81f94e85b2595fbf11ce19ac4e95c650318fa
Alexis Abda <aabda@student.s19.be>
2022-07-18 18:05:02 +0200

    Fix cast

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 13:47:47 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/18 16:44:34 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 17:57:15 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memset(void *b, int c, size_t len)
 	str = (unsigned char *)b;
 	while (i < len)
 	{
-		str[i] = (unsigned char *)c;
+		str[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
