94519d77757cab6b93650332273a067aa1264d17
Alexis Abda <aabda@student.s19.be>
2022-07-18 10:23:40 +0200

    Protect ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 14:06:13 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/10 16:05:22 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/18 10:06:24 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	// if (!src || !dst)
-	// 	return (NULL);
+	if (!src && !dst)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned const char *)src)[i];
