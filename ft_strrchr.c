6e5acc6fa4fae59429726205fe359f046ceaa9d2
Alexis Abda <aabda@student.s19.be>
2022-07-20 17:27:23 +0200

    Fix for tripouille test and totally change the logic code

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aabda <aabda@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 15:19:51 by aabda             #+#    #+#             */
-/*   Updated: 2022/07/18 14:47:56 by aabda            ###   ########.fr       */
+/*   Updated: 2022/07/20 17:24:27 by aabda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		i;
+	size_t	i;
+	size_t	position;
 
-	i = (ft_strlen(s));
-	while (i >= 0)
+	i = 0;
+	position = 0;
+	while (s[i])
 	{
-		if (s[i] == c)
-			return ((char *)&s[i]);
-		i--;
+		if ((unsigned char)s[i] == (unsigned char)c)
+			position = i;
+		i++;
 	}
+	if (position > 0 || (unsigned char)s[0] == (unsigned char)c)
+		return ((char *)&s[position]);
 	if (s[i] == 0 && c == 0)
 		return ((char *)&s[i]);
 	return (NULL);
